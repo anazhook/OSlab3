16aa20d72228c19750f2ccc05d32b286540fe5bc
Anastasiya <125749225+anazhook@users.noreply.github.com>
2023-12-07 02:00:14 +0300

    Update marker.cpp

--- marker.cpp
+++ marker.cpp
@@ -1,130 +1,211 @@
 #include <iostream>
-#include <windows.h>
-#include <semaphore>
-#include <mutex>
+#include <string>
+#include <Windows.h>
 #include <vector>
 
-std::mutex m;
+CRITICAL_SECTION cs;
+HANDLE* hStartMarker;
+HANDLE* hMarkerCantResume;
+HANDLE* hMarkerRestart;
+HANDLE* hMarkerStop;
+HANDLE hMarkerStopped;
 
-class semaphore
+int* Array;
+int arraysize;
+
+void ThreadMarker(LPVOID param)
 {
-    unsigned int count_;
-    std::mutex mutex_;
-    std::condition_variable condition_;
+	int number = (int)param;
 
-public:
-    explicit semaphore(unsigned int initial_count) : count_(initial_count), mutex_(), condition_() {}
+	WaitForMultipleObjects(arraysize,hStartMarker, TRUE, INFINITE);
+	ResetEvent(hStartMarker[number - 1]);
 
-    void signal() //aka release
-    {
-        std::unique_lock<std::mutex> lock(mutex_);
+	srand(number);
+	int count = 0;
+	std::vector<int> changedIndexes;
 
-        ++count_;
-        condition_.notify_one();
-    }
+	while (true)
+	{
+		int randomNuber = std::rand();
+		int modul = randomNuber % arraysize;
+		EnterCriticalSection(&cs);
+		if (Array[modul] == 0)
+		{
+			Sleep(5);
+			Array[modul] = number;
+			changedIndexes.push_back(modul);
+			LeaveCriticalSection(&cs);
+			Sleep(5);
+			count++;
+		}
+		else
+		{
+			LeaveCriticalSection(&cs);
+			std::cout << "\nNumber of the thread: " << number << "\n";
+			std::cout << "Number of marked elements: " << count << "\n";
+			std::cout << "Index of impossible to mark element: " << modul << "\n";
 
-    void wait() //aka acquire
-    {
-        std::unique_lock<std::mutex> lock(mutex_);
-        while (count_ == 0) condition_.wait(lock);
-        --count_;
-    }
+			HANDLE events[2] = { hMarkerRestart[number - 1], hMarkerStop[number - 1] };
+			SetEvent(hMarkerCantResume[number - 1]);
+			DWORD result = WaitForMultipleObjects(2, events, FALSE, INFINITE);
+			{
+				if (result == 0)
+				{
+					ResetEvent(hMarkerRestart[number - 1]);
+				}
+				else if (result == 1)
+				{
+					for (int i = 0; i < changedIndexes.size(); i++)
+					{
+						EnterCriticalSection(&cs);
+						Array[changedIndexes[i]] = 0;
+						LeaveCriticalSection(&cs);
+					}
+					ResetEvent(hMarkerStop[number - 1]);
+					SetEvent(hMarkerStopped);
+					changedIndexes.clear();
+					return;
+				}
+				else
+				{
+					LeaveCriticalSection(&cs);
+					std::cerr << "Wrong Response\n";
+					return;
+				}
+			}
+		}
+	}
 
-    semaphore() {
-        count_ = 0;
-    }
-};
-HANDLE hstop_sem = CreateSemaphore(NULL, 0, 1, NULL);//
-HANDLE* hmarker_sem;
-semaphore pause_sem(0);
-semaphore* marker_sems;
 
-void first(int i, std::vector<int>& v, std::vector<bool>& marked) {
-    marker_sems[i].wait();
-    WaitForSingleObject(hmarker_sem[i], INFINITE);//
-    srand(i);
-    int c = 0;
-    while (true) {
-        int temp = rand() % v.size();
-        m.lock();
-        if (v[temp] == 0) {
-            Sleep(5);
-            v[temp] = i;
-            Sleep(5);
-            c++;
-            m.unlock();
-        }
-        else {
-            std::cout << "\n serial number: " << i << ", number of marked elements: " << c << ", can't be marked: " << temp;
-            m.unlock();
-            //pause_sem.signal();
-            ReleaseSemaphore(hstop_sem, 1, 0);//
-            WaitForSingleObject(hmarker_sem[i], INFINITE);//
-            marker_sems[i].wait();
-            if (marked[i]) {
-                m.lock();
-                for (int j = 0; j < v.size(); j++) 
-                    if (v[j] == i) 
-                        v[j] = 0;
-                m.unlock();
-                marker_sems[i].signal();
-                ReleaseSemaphore(hmarker_sem, 1, 0); //
-                break;
-            }
-        }
-    }
 }
 
-int main() {
-    int n, k, p = 0;
-    std::cout << "array size: ";
-    std::cin >> n;
-    std::vector<int> v(n);
-    std::cout << "number of 'marker' threads: ";
-    std::cin >> k;
-    int kn = k;
-    HANDLE** hmarkers = new HANDLE*[k + 1];//
-    std::thread** marker = new std::thread*[k + 1];
+int main()
+{
+	//int arraysize
+	std::cout << "Size of array: ";
+	std::cin >> arraysize;
 
-    std::vector<bool> marked(k + 1);
-    marked[0] = true;
-    hmarker_sem = new HANDLE[k + 1]; //
-    marker_sems = new semaphore[k + 1];
-    for (int i = 1; i < k + 1; i++) {
-        marker[i] = new std::thread(first, i, std::ref(v), std::ref(marked));
-        marker[i]->detach();
-        marker_sems[i].signal();
-        ReleaseSemaphore(hmarkers, 1, 0);//
-    }
-    while (true) {
-        for (int i = 1; i < kn + 1; i++) {
-            WaitForSingleObject(hstop_sem, INFINITE);//
-            //pause_sem.wait();
-        }
-        std::cout << std::endl;
-        m.lock();
-        for (int i = 0; i < n; i++) 
-            std::cout << v[i] << " ";
-        m.unlock();
-        std::cout << std::endl;
-        int del;
-        std::cout << " marker to delete: "; 
-        std::cin >> del;
-        marked[del] = true;
-        p++;
-        ReleaseSemaphore(hmarker_sem[del], 1, 0);//
-        marker_sems[del].signal();
-        Sleep(1);
-        WaitForSingleObject(hmarker_sem, INFINITE);//
-        marker_sems[del].wait();
-        kn--;
-        for (int i = 0; i < n; i++) 
-            std::cout << v[i] << " ";
-        for (int i = 1; i < k + 1; i++) {
-            marker_sems[i].signal();
-            ReleaseSemaphore(hmarker_sem, 1, 0);//
-        }
-        if (p == k) break;
-    }
-    return 0;
+	//arraysize = arraysize
+	Array = new int[arraysize];
+	for (int i = 0; i < arraysize; i++)
+		Array[i] = 0;
+
+	InitializeCriticalSection(&cs);
+	int threadsAmount;
+	std::cout << "number of marker threads: ";
+	std::cin >> threadsAmount;
+
+	HANDLE* hThread = new HANDLE[threadsAmount];
+	DWORD* dwThread = new DWORD[threadsAmount];
+	hMarkerRestart = new HANDLE[threadsAmount];
+	hMarkerStop = new HANDLE[threadsAmount];
+	hMarkerCantResume = new HANDLE[threadsAmount];
+	hStartMarker = new HANDLE[threadsAmount];
+	int* indexes = new int[threadsAmount];
+
+
+	for (int i = 0; i < threadsAmount; i++)
+	{
+		indexes[i] = i + 1;
+	}
+
+	for (int i = 0; i < threadsAmount; i++)
+	{
+		hMarkerRestart[i] = CreateEvent(NULL, FALSE, FALSE, NULL);
+		hMarkerStop[i] = CreateEvent(NULL, FALSE, FALSE, NULL);
+		hMarkerCantResume[i] = CreateEvent(NULL, FALSE, FALSE, NULL);
+		hStartMarker[i] = CreateEvent(NULL, TRUE, FALSE, NULL);
+	}
+	hMarkerStopped = CreateEvent(NULL, FALSE, FALSE, NULL);
+
+	std::vector<int> Markers;
+	for (int i = 0; i < threadsAmount; i++)
+		Markers.push_back(i);
+
+	for (int i = 0; i < threadsAmount; i++)
+	{
+		hThread[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ThreadMarker, (void*)indexes[i], 0, &dwThread[i]);
+	}
+
+
+
+	for (int i = 0; i < threadsAmount; i++)
+	{
+		SetEvent(hStartMarker[i]);
+	}
+
+
+	while (true)
+	{
+		for (int i = 0; i < Markers.size(); i++)
+			if (Markers[i] == -1)
+				SetEvent(hMarkerCantResume[i]);
+		if (WaitForMultipleObjects(threadsAmount, hMarkerCantResume, TRUE, INFINITE) == WAIT_FAILED)
+		{
+			std::cout << "Wait for multiple objects failed." << std::endl;
+			std::cout << "Press any key to exit." << std::endl;
+		}
+		for (int i = 0; i < Markers.size(); i++)
+			ResetEvent(hMarkerCantResume[i]);
+		std::cout << "Array after Markers' work:\n";
+		for (int i = 0; i < arraysize; i++)
+		{
+			std::cout << Array[i] << " ";
+		}
+		std::cout << "\n";
+		
+		std::cout << "working markers: \n";
+		for (int i = 0; i < Markers.size(); i++)
+			if (Markers[i] != -1)
+				std::cout << Markers[i] << " ";
+		std::cout << "\n";
+		std::cout << "Enter the index of the thread to be stopped:\n";
+		int stoppedThread;
+		std::cin >> stoppedThread;
+
+		for (int i = 0; i < Markers.size(); i++)
+		{
+			if (Markers[i] == stoppedThread)
+				Markers[i] = -1;
+		}
+
+		SetEvent(hMarkerStop[stoppedThread]);
+		WaitForSingleObject(hMarkerStopped, INFINITE);
+		ResetEvent(hMarkerStopped);
+
+
+		std::cout << "Array after stopping " << stoppedThread <<"th thread: \n";
+		for (int i = 0; i < arraysize; i++)
+		{
+			std::cout << Array[i] << " ";
+		}
+		std::cout << "\n";
+
+		bool flag = false;
+		for (int i = 0; i < Markers.size(); i++)
+		{
+			if (Markers[i] != -1)
+				flag = true;
+		}
+
+		if (flag == false)
+		{
+			std::cout << "All Markers are closed\n";
+			for (int i = 0; i < threadsAmount; i++)
+			{
+				std::cout << "Closing handle " << i << "\n";
+				CloseHandle(hThread[i]);
+			}
+			DeleteCriticalSection(&cs);
+			return 0;
+		}
+
+		for (int i = 0; i < Markers.size(); i++)
+		{
+			if (Markers[i] != -1)
+			{
+				SetEvent(hMarkerRestart[i]);
+			}
+		}
+	}
 }
